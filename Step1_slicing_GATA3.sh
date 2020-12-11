2864569c1a0bc83fa045bd418903d298489e7df7
Raul-Nicolas <rmateosncc@gmail.com>
2020-12-11 05:20:47 +0000

    Fixed BAMslicer step for each gene

--- Step1_slicing_GATA3.sh
+++ Step1_slicing_GATA3.sh
@@ -7,7 +7,7 @@ i) INPUTPATH=${OPTARG};;
 o) OUTPUTPATH=${OPTARG};;
 esac
 done
- cat   ${INPUTPATH}/samplelist.txt | while read SAMPLE
+cat   samplelist | while read SAMPLE
 do 
  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr10:8054810-8074890" >${OUTPUTPATH}${SAMPLE}_GATA3.bam
  samtools index ${OUTPUTPATH}${SAMPLE}_GATA3.bam
--- Step1_slicing_PIK3R1.sh
+++ Step1_slicing_PIK3R1.sh
@@ -7,7 +7,7 @@ i) INPUTPATH=${OPTARG};;
 o) OUTPUTPATH=${OPTARG};;
 esac
 done
- cat   ${INPUTPATH}/samplelist.txt | while read SAMPLE
+cat   samplelist | while read SAMPLE
 do 
  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr5:68215756-68301821" > ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
  samtools index ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
