37045a11237c7d99a7bc7bea85d552badda3dffe
Raul-Nicolas <rmateosncc@gmail.com>
2020-12-17 00:20:40 +0000

    Step_1 is now only one script, not one for each gene

--- FULL_PIPE.sh
+++ FULL_PIPE.sh
@@ -15,7 +15,7 @@ mkdir metadata
 mkdir output_intronretention
 mkdir tmp
 ls ${INPUTFOLDER} | grep .bam | grep -v .bai | grep -Po '.*(?=\.)' > samplelist
-./Step1_slicing_${GENE}.sh -i ${INPUTFOLDER} -o slicedbam/
+./Step1_slicing.sh -i ${INPUTFOLDER} -o slicedbam/ -g ${GENE}
 Rscript ./Step2_SJmaker.R slicedbam/ output_SJ/  
 ./Step3_intronretention.sh -i slicedbam/ -o output_intronretention/
 Rscript ./Step4_SJ_selection.R output_SJ/ tmp/ ${GENE}
---
+++ Step1_slicing.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+while getopts i:o:g: option
+do
+case "${option}"
+in
+i) INPUTPATH=${OPTARG};;
+o) OUTPUTPATH=${OPTARG};;
+g) GENE=${OPTARG};;
+esac
+done
+cat   samplelist | while read SAMPLE
+do 
+ if [[ “$GENE” == “TP53” ]]
+ then
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr17:7668402-7687538" > ${OUTPUTPATH}${SAMPLE}_TP53.bam
+  samtools index ${OUTPUTPATH}${SAMPLE}_TP53.bam
+ elif [[ “$GENE” == “CDKN2A”  ]]
+ then
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr9:21967753-21975098" > ${OUTPUTPATH}${SAMPLE}_CDKN2A.bam
+  samtools index ${OUTPUTPATH}${SAMPLE}_CDKN2A.bam
+ elif [[ “$GENE” == “PIK3R1”  ]]
+ then
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr5:68215756-68301821" > ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
+  samtools index ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
+ elif [[ “$GENE” == “GATA3”  ]]
+ then
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr10:8054810-8074890" >${OUTPUTPATH}${SAMPLE}_GATA3.bam
+  samtools index ${OUTPUTPATH}${SAMPLE}_GATA3.bam
+ fi
+done
