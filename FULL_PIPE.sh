a11ff9d88ef772c6bce5713a7b24a7b91815d108
Raul-Nicolas <rmateosncc@gmail.com>
2020-12-17 06:54:22 +0000

    FULL_PIPE handles wrong gene name and non-existant path

--- FULL_PIPE.sh
+++ FULL_PIPE.sh
@@ -8,23 +8,34 @@ g) GENE=${OPTARG};;
 i) INPUTFOLDER=${OPTARG};;
 esac
 done
-mkdir output_SJ
-mkdir slicedbam
-mkdir output_intronretention_prepared 
-mkdir metadata
-mkdir output_intronretention
-mkdir tmp
-ls ${INPUTFOLDER} | grep .bam | grep -v .bai | grep -Po '.*(?=\.)' > samplelist
-./Step1_slicing.sh -i ${INPUTFOLDER} -o slicedbam/ -g ${GENE}
-Rscript ./Step2_SJmaker.R slicedbam/ output_SJ/  
-./Step3_intronretention.sh -i slicedbam/ -o output_intronretention/
-Rscript ./Step4_SJ_selection.R output_SJ/ tmp/ ${GENE}
-Rscript ./Step5_Intron_preparation.R output_intronretention/ metadata/ tmp/ ${GENE}
-Rscript ./Step6_5motif_betabinomial.R tmp/ ${GENE}
-rm -r slicedbam
-rm -r output_SJ
-rm -r output_intronretention
-rm -r tmp
-rm -r output_intronretention_prepared
-rm -r metadata
-rm samplelist
+if [ ! -d "$INPUTFOLDER" ]; then
+ echo ERROR: $INPUTFOLDER folder does no exist.
+ exit 1 # terminate and indicate error
+fi
+
+if [ "$GENE" == 'TP53' ] || [ "$GENE" == 'TP53' ] || [ "$GENE" == 'TP53' ] || [ "$GENE" == 'TP53' ]
+then
+ mkdir output_SJ
+ mkdir slicedbam
+ mkdir output_intronretention_prepared 
+ mkdir metadata
+ mkdir output_intronretention
+ mkdir tmp
+ ls ${INPUTFOLDER} | grep .bam | grep -v .bai | grep -Po '.*(?=\.)' > samplelist
+ ./Step1_slicing.sh -i ${INPUTFOLDER} -o slicedbam/ -g ${GENE}
+ Rscript ./Step2_SJmaker.R slicedbam/ output_SJ/  
+ ./Step3_intronretention.sh -i slicedbam/ -o output_intronretention/
+ Rscript ./Step4_SJ_selection.R output_SJ/ tmp/ ${GENE}
+ Rscript ./Step5_Intron_preparation.R output_intronretention/ metadata/ tmp/ ${GENE}
+ Rscript ./Step6_5motif_betabinomial.R tmp/ ${GENE}
+ rm -r slicedbam
+ rm -r output_SJ
+ rm -r output_intronretention
+ rm -r tmp
+ rm -r output_intronretention_prepared
+ rm -r metadata
+ rm samplelist
+ else
+ echo ERROR: $GENE is not an allowed gene name. Please check spelling.
+ exit 1 # terminate and indicate error
+fi
--- Step1_slicing.sh
+++ Step1_slicing.sh
@@ -10,24 +10,21 @@ esac
 done
 cat   samplelist | while read SAMPLE
 do 
- if [[ “$GENE” == “TP53” ]]
+ if [ “$GENE” == “TP53” ]]
  then
   samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr17:7668402-7687538" > ${OUTPUTPATH}${SAMPLE}_TP53.bam
   samtools index ${OUTPUTPATH}${SAMPLE}_TP53.bam
- elif [[ “$GENE” == “CDKN2A”  ]]
+ elif [ “$GENE” == “CDKN2A”  ]
  then
   samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr9:21967753-21975098" > ${OUTPUTPATH}${SAMPLE}_CDKN2A.bam
   samtools index ${OUTPUTPATH}${SAMPLE}_CDKN2A.bam
- elif [[ “$GENE” == “PIK3R1”  ]]
+ elif [ “$GENE” == “PIK3R1” ]
  then
   samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr5:68215756-68301821" > ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
   samtools index ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
- elif [[ “$GENE” == “GATA3”  ]]
+ elif [ “$GENE” == “GATA3”  ]
  then
   samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr10:8054810-8074890" >${OUTPUTPATH}${SAMPLE}_GATA3.bam
   samtools index ${OUTPUTPATH}${SAMPLE}_GATA3.bam
- else
-  echo ERROR: $GENE is not an allowed gene name. Please check spelling.
-  exit 1 # terminate and indicate error
  fi
 done
