73319c9d1eb9a78e76dfa4a20907d727e3ca8aab
Raul-Nicolas <rmateosncc@gmail.com>
2020-12-18 04:21:12 +0000

    Step_1 doesnt creat underscore in names

--- Step1_slicing.sh
+++ Step1_slicing.sh
@@ -12,19 +12,19 @@ cat   samplelist | while read SAMPLE
 do 
  if [ “$GENE” == “TP53” ]
  then
-  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr17:7668402-7687538" > ${OUTPUTPATH}${SAMPLE}_TP53.bam
-  samtools index ${OUTPUTPATH}${SAMPLE}_TP53.bam
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr17:7668402-7687538" > ${OUTPUTPATH}${SAMPLE}.bam
+  samtools index ${OUTPUTPATH}${SAMPLE}.bam
  elif [ “$GENE” == “CDKN2A”  ]
  then
-  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr9:21967753-21975098" > ${OUTPUTPATH}${SAMPLE}_CDKN2A.bam
-  samtools index ${OUTPUTPATH}${SAMPLE}_CDKN2A.bam
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr9:21967753-21975098" > ${OUTPUTPATH}${SAMPLE}.bam
+  samtools index ${OUTPUTPATH}${SAMPLE}.bam
  elif [ “$GENE” == “PIK3R1” ]
  then
-  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr5:68215756-68301821" > ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
-  samtools index ${OUTPUTPATH}/${SAMPLE}_PIK3R1.bam
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr5:68215756-68301821" > ${OUTPUTPATH}/${SAMPLE}.bam
+  samtools index ${OUTPUTPATH}/${SAMPLE}.bam
  elif [ “$GENE” == “GATA3”  ]
  then
-  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr10:8054810-8074890" >${OUTPUTPATH}${SAMPLE}_GATA3.bam
-  samtools index ${OUTPUTPATH}${SAMPLE}_GATA3.bam
+  samtools view -b ${INPUTPATH}${SAMPLE}.bam "chr10:8054810-8074890" >${OUTPUTPATH}${SAMPLE}.bam
+  samtools index ${OUTPUTPATH}${SAMPLE}.bam
  fi
 done
